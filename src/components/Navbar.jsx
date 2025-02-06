692f14ba5d9c8aae05a15ebc39e253c20113f1ea
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2025-02-06 17:21:29 +0500

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <div w-full>
-      <img className="w-full h-20" src="./public/Combined Shape.png" alt="" />
+      <img className="w-full h-20" src="../public/Combined Shape.png" alt="" />
     </div>
   );
 }
